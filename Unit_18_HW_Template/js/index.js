2a77a5db0dd4ef0ad615ca297eeb70d4f4756e0a
derikiverson <derikiverson@gmail.com>
2021-05-06 15:50:50 -0500

    Ready to turn in for HW

---
+++ Unit_18_HW_Template/js/index.js
@@ -0,0 +1,6 @@
+console.log("Your index.js file is loading correctly!");
+    $("li").hover(function(){
+      $(this).css("background-color", "ease-in-out");
+      }, function(){
+      $(this).css("background-color", "none");
+    });
